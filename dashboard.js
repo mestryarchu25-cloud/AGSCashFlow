5c2cc1bc237c7d55be19dc4d3ab245c821639848
mestryarchu25-cloud <mestryarchu25@gmail.com>
2026-09-10 12:47:52 +0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,16 @@
+fetch("cashflow.json")
+    .then(response => response.json())
+    .then(data => {
 
+        document.getElementById("dashboard").innerHTML =
+            "<h2>Cashflow Data Loaded</h2>" +
+            "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+
+    })
+    .catch(error => {
+
+        document.getElementById("dashboard").innerHTML =
+            "<p>Unable to load cashflow data.</p>";
+
+        console.error(error);
+    });
